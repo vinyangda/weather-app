91f0fcb9edb5937810f09cef198e5ee19a73def6
vin <didekqls93@gmail.com>
2023-02-16 21:02:27 +0900

    weather :day 1 마무리

--- src/components/WeatherButton.js
+++ src/components/WeatherButton.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import styled from "styled-components";
 
 const WeatherButton = () => {
   return (
-    <div>
-      <button>button</button>
-    </div>
+    <ButtonWrap>
+      <CurrentButton>Current</CurrentButton>
+      <OtherButton>Ohio</OtherButton>
+      <OtherButton>Seoul</OtherButton>
+    </ButtonWrap>
   );
 };
 
 export default WeatherButton;
+const ButtonWrap = styled.div`
+  margin: 50px;
+`;
+const CurrentButton = styled.button`
+  background-color: rgba(1, 1, 1, 0.5);
+  border: 1px solid white;
+  margin: 10px;
+`;
+const OtherButton = styled.button`
+  background-color: rgba(1, 1, 1, 0.1);
+  border: 1px solid white;
+  margin: 10px;
+`;
